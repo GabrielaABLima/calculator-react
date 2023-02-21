0fea28672a47be54f5580fa1acba05fee745e6ec
GabrielaABLima <gabriela.andrade.bueno.lima@gmail.com>
2023-02-20 21:24:49 -0300

    React app creation

---
+++ calculator-app/src/App.js
@@ -0,0 +1,14 @@
+import Wrapper from "./components/Wrapper";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <h1>App</h1>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
